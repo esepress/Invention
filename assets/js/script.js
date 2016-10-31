09243543a0e8b4578687268110f7c140e427f143
mnkakon <mahmudunnobi11@gmail.com>
2016-10-31 19:58:33 +0600

    slide header change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-/*$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    margin:10,
-    loop:true,
-    autoWidth:true,
-    items:4
-})
-});*//*
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})*/
 
 $(document).ready(function () {
         // Menu.
